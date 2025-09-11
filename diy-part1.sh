0a38155a12b5abe130758a9f1a99b33bd3235b19
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-11 15:51:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -35,13 +35,6 @@ cat >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts << EOF
 };
 EOF
 
-#echo '&xhci {' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-#echo '        mediatek,u3p-dis-msk = <0x0>;' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-#echo '        phys = <&u2port0 PHY_TYPE_USB2>,' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-#echo '                   <&u3port0 PHY_TYPE_USB3>;' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-#echo '        status = "okay";' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-#echo '};' >> target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
-
 #添加K2P 32M nand USB支持
 sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M USB)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
