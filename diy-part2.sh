5ec61837b64b0e70a73696eff3fd5fcf7eb58569
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-25 20:41:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,11 +20,11 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 ##-----------------Del duplicate packages------------------
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/luci/applications/luci-app-appfilter
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/open-app-filter
+#rm -rf feeds/luci/applications/luci-app-appfilter
+#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+#find ./ | grep Makefile | grep mosdns | xargs rm -f
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
